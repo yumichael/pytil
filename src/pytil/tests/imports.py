406f5ced67883f3e8abf0793cbfa90b945122a69
Michael Yu <myy2357@gmail.com>
2026-01-11 21:40:21 -0800

    Add testing scratchpad record from other project

---
+++ src/pytil/tests/imports.py
@@ -0,0 +1,51 @@
+import math
+import numbers
+import os
+import pickle
+import random
+import re
+import shutil
+import sys
+import time
+from collections import Counter, deque, namedtuple
+from collections.abc import Sequence
+from copy import deepcopy
+from enum import Enum, IntEnum, auto
+from functools import cache
+from itertools import chain, pairwise, product
+from math import atan2, ceil, gcd, prod
+from numbers import Number
+from pathlib import Path
+from timeit import timeit
+from typing import Literal
+
+import cmasher as cmr
+import imageio
+import imageio.v3 as iio
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numba as nb
+import numpy as np
+from hilbert import decode, encode
+from numba import njit, prange, vectorize
+from numba.experimental import jitclass
+from numba.typed import List
+from numba.types import Tuple
+from PIL import Image, ImageOps
+
+from pytil.data_structures.array_deque import get_array_deque_1d_items_jitclass
+from pytil.data_structures.array_heap import get_array_heap_1d_items_jitclass
+from pytil.data_structures.array_stack import get_array_stack_1d_items_jitclass
+from pytil.data_structures.array_treap import get_array_treap_1d_items_jitclass
+from pytil.image_utility import upscale_image
+from pytil.new_utility import (
+    cardinal_directions,
+    highest_power_of_2_dividing,
+    is_power_of_two,
+    ordinal_directions,
+    tau,
+)
+from pytil.numba_utility import set_seed
+from pytil.object import Namespace as O
+from pytil.quickvis import view
+from pytil.utility import closure
